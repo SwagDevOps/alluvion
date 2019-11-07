e6a7408198b28285ebeaef7bd52ce556a74851d7
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-11-07 10:09:25 +0100

    alluvion_spec (spec) add Config const example

--- spec/alluvion_spec.rb
+++ spec/alluvion_spec.rb
@@ -3,6 +3,7 @@
 # constants ---------------------------------------------------------
 describe Alluvion, :alluvion do
   it { expect(described_class).to be_const_defined(:VERSION) }
+  it { expect(described_class).to be_const_defined(:Config) }
   it { expect(described_class).to be_const_defined(:Host) }
   it { expect(described_class).to be_const_defined(:URI) }
 end
