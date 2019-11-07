159cfaad35543b213a430c9c1ddb36dc71031db2
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-11-07 10:02:54 +0100

    uri_spec (spec) minor changes

--- spec/alluvion/uri_spec.rb
+++ spec/alluvion/uri_spec.rb
@@ -3,14 +3,13 @@
 describe Alluvion::URI, :'alluvion/uri' do
   let(:subject) { described_class.new('ssh://user@example.org:22') }
 
-  it { expect(subject).to respond_to(:host).with(0).arguments }
-  it { expect(subject).to respond_to(:hostname).with(0).arguments }
-
   %w[scheme user query fragment path port].sort.each do |method_name|
     it { expect(subject).to respond_to(method_name).with(0).arguments }
   end
 
   %w[host hostname].sort.each do |method_name|
+    it { expect(subject).to respond_to(method_name).with(0).arguments }
+
     context ".#{method_name}" do
       it { expect(subject.public_send(method_name)).to be_a(String) }
       it { expect(subject.public_send(method_name)).to be_a(Alluvion::Host) }
