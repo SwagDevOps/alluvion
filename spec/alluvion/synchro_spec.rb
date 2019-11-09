72d022965588f1b3d3a814480488ecc8ff8fefe7
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-11-10 00:56:37 +0100

    synchro_spec (spec) introduced

---
+++ spec/alluvion/synchro_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+describe Alluvion::Synchro, :'alluvion/synchro' do
+  let(:subject) { described_class.new(sham!(:configs).complete) }
+
+  [:up, :down].sort.each do |method_name|
+    it { expect(subject).to respond_to(method_name).with(0).arguments }
+
+    context ".#{method_name}" do
+      it do
+        # rubocop:disable Metrics/LineLength
+        %r{^Can not connect to "[a-z]+://[a-z]+@[a-z]+.[a-z]+:[0-9]+"$}.tap do |msg|
+          expect { subject.public_send(method_name) }.to raise_error(RuntimeError).with_message(msg)
+        end
+        # rubocop:enable Metrics/LineLength
+      end
+    end
+  end
+end
