3c4f3e5fa9320de66a26dd23ee186b4b22ae69af
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-11-10 18:03:55 +0100

    sham (spec) configs rewrite + add locks

--- spec/sham/configs.rb
+++ spec/sham/configs.rb
@@ -2,32 +2,38 @@
 
 autoload(:SecureRandom, 'securerandom')
 
-Sham.config(FactoryStruct, :configs) do |c|
-  # noinspection RubyStringKeysInHashInspection
-  c.attributes do
-    {
-      complete: lambda do
-        # @formatter:off
-        {
-          'url' => 'ssh://user@example.org:22',
-          'timeout' => 0.5,
-          'todo' => {
-            'extensions' => ['torrent'],
-            'mime_types' => ['application/x-bittorrent'],
-          },
-          'paths' => {
-            'local' => {
-              'done' => '/data/complete',
-              'todo' => SAMPLES_PATH.join('files'),
-            },
-            'remote' => {
-              'done' => '/var/user/complete',
-              'todo' => '/var/user/todo',
-            }
-          }
-        }
-        # @formatter:off
-      end.call
+# @formatter:off
+# noinspection RubyStringKeysInHashInspection
+{
+  complete: {
+    'url' => 'ssh://user@example.org:22',
+    'timeout' => 0.5,
+    'todo' => {
+      'extensions' => ['torrent'],
+      'mime_types' => ['application/x-bittorrent'],
+    },
+    'locks' => {
+      'up' => SPEC_DIR.join('temp/lock/up.lock'),
+      'down' => SPEC_DIR.join('temp/lock/down.lock'),
+    },
+    'paths' => {
+      'local' => {
+        'done' => '/data/complete',
+        'todo' => SAMPLES_PATH.join('files'),
+      },
+      'remote' => {
+        'done' => '/var/user/complete',
+        'todo' => '/var/user/todo',
+      }
     }
+  }
+}.yield_self do |config|
+  Sham.config(FactoryStruct, :configs) do |c|
+    c.attributes do
+      {
+        complete: config[:complete]
+      }
+    end
   end
 end
+# @formatter:on
