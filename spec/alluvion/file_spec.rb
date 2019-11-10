f218270c3766f4921ded9434d0ec2dbff366b7ea
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-11-10 01:03:23 +0100

    file_spec (spec) minor changes

--- spec/alluvion/file_spec.rb
+++ spec/alluvion/file_spec.rb
@@ -3,7 +3,9 @@
 describe Alluvion::File, :'alluvion/file' do
   sham!(:samples).files.each do |fp, mime_type|
     context '.mime_type' do
-      it { expect(described_class.new(fp).mime_type).to eq(mime_type) }
+      let(:subject) { described_class.new(fp) }
+
+      it { expect(subject.mime_type).to eq(mime_type) }
     end
   end
 end
