72b68d22312ad8fcf642d0ecb01591bc32669ecd
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-11-12 09:52:20 +0100

    sham (spec) samples add configs

--- spec/sham/samples.rb
+++ spec/sham/samples.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 autoload(:Pathname, 'pathname')
+autoload(:YAML, 'yaml')
 
 Sham.config(FactoryStruct, :samples) do |c|
   c.attributes do
@@ -13,6 +14,18 @@ Sham.config(FactoryStruct, :samples) do |c|
               res[1] = 'text/html' if fname == '404'
               res[1] = 'empty' if fname == 'empty'
             end
+          end
+        end
+      end.call.to_h,
+      configs: lambda do
+        # Get sample configs
+        # indexed by filenamewith path and read (parsed) content
+        Dir.glob(SAMPLES_PATH.join('configs', '*')).map do |file|
+          [Pathname.new(file).basename('.*').to_s, nil].tap do |res|
+            {
+              file: Pathname.new(file),
+              read: YAML.safe_load(Pathname.new(file).read)
+            }.tap { |struct| res[1] = FactoryStruct.new(struct) }
           end
         end
       end.call.to_h
