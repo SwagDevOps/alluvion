9f81aa2b944bded114cc00c06756d34113194437
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-11-25 09:04:57 +0100

    alluvion_spec (spec) add Shell const example

--- spec/alluvion_spec.rb
+++ spec/alluvion_spec.rb
@@ -6,6 +6,7 @@ describe Alluvion, :alluvion do
   it { expect(described_class).to be_const_defined(:Config) }
   it { expect(described_class).to be_const_defined(:File) }
   it { expect(described_class).to be_const_defined(:Host) }
+  it { expect(described_class).to be_const_defined(:Shell) }
   it { expect(described_class).to be_const_defined(:Synchro) }
   it { expect(described_class).to be_const_defined(:URI) }
 end
