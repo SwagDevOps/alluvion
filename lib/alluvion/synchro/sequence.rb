5955245fe726bb7348e52a408fce83397cdb74ee
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-12-15 16:32:16 +0100

    sequence (synchro) introduced

---
+++ lib/alluvion/synchro/sequence.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# Copyright (C) 2017-2019 Dimitri Arrigoni <dimitri@arrigoni.me>
+# License GPLv3+: GNU GPL version 3 or later
+# <http://www.gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+
+require_relative '../synchro'
+
+# Describe a sequence of commands
+class Alluvion::Synchro::Sequence < Array
+  # @param [Array<Alluvion::Synchro::Command>|Array<String>] commands
+  def initialize(commands)
+    commands.map do |command|
+      Alluvion::Synchro::Command.new(command)
+    end.yield_self do |args|
+      super(args)
+    end
+  end
+
+  def call
+    # @todo implement sequence execution
+  end
+end
