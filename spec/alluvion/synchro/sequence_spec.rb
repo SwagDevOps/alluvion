128a70af7748809007f7534fa86cc7ca30bc1792
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-12-17 07:04:43 +0100

    synchro (spec) sequence class method spec example added

--- spec/alluvion/synchro/sequence_spec.rb
+++ spec/alluvion/synchro/sequence_spec.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-# constants ---------------------------------------------------------
+# constants + class methods ---------------------------------------------------
 describe Alluvion::Synchro::Sequence, :'alluvion/synchro/sequence' do
   it { expect(described_class).to be_const_defined(:Command) }
   it { expect(described_class).to be_const_defined(:Factory) }
+
+  it { expect(described_class).to respond_to(:build).with(2).arguments }
 end
