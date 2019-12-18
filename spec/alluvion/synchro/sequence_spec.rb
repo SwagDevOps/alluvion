3b73c3c87c7aeac725e0cfd3081decb2f56f324b
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-12-18 17:05:48 +0100

    synchro (spec) add example for empty constructor + Object#call example

--- spec/alluvion/synchro/sequence_spec.rb
+++ spec/alluvion/synchro/sequence_spec.rb
@@ -5,10 +5,17 @@ describe Alluvion::Synchro::Sequence, :'alluvion/synchro/sequence' do
   it { expect(described_class).to be_const_defined(:Command) }
   it { expect(described_class).to be_const_defined(:Factory) }
 
+  it { expect(described_class).to respond_to(:new).with(0).arguments }
   it { expect(described_class).to respond_to(:new).with(1).arguments }
   it { expect(described_class).to respond_to(:build).with(2).arguments }
 end
 
+# instance methods ------------------------------------------------------------
+describe Alluvion::Synchro::Sequence, :'alluvion/synchro/sequence' do
+  it { expect(subject).to respond_to(:call).with(0).arguments }
+end
+
+# .build() minimal example ----------------------------------------------------
 describe Alluvion::Synchro::Sequence, :'alluvion/synchro/sequence' do
   context '.build(:up, config)' do
     sham!(:configs).complete.tap do |config|
