29a7b936a3035a7e75806cf45859a083dcb40bed
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-12-18 07:02:01 +0100

    synchro (spec) sequence_spec add example for .build() return type

--- spec/alluvion/synchro/sequence_spec.rb
+++ spec/alluvion/synchro/sequence_spec.rb
@@ -7,3 +7,14 @@ describe Alluvion::Synchro::Sequence, :'alluvion/synchro/sequence' do
 
   it { expect(described_class).to respond_to(:build).with(2).arguments }
 end
+
+describe Alluvion::Synchro::Sequence, :'alluvion/synchro/sequence' do
+  sham!(:configs).complete.tap do |config|
+    subject { described_class.build(:up, config) }
+  end
+
+  context '.build(:up, config)' do
+    it { expect(subject).to be_a(Array) }
+    it { expect(subject).to be_a(Alluvion::Synchro::Sequence) }
+  end
+end
