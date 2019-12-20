00d91d5b5f79ae85ba9a4cf88ef7b7c20a71cc3a
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-12-20 09:28:45 +0100

    sequence (synchro) not_found_error introduced

---
+++ lib/alluvion/synchro/sequence/not_found_error.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# Copyright (C) 2017-2019 Dimitri Arrigoni <dimitri@arrigoni.me>
+# License GPLv3+: GNU GPL version 3 or later
+# <http://www.gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+
+require_relative '../sequence'
+
+# No entry was found in the factory.
+class Alluvion::Synchro::Sequence::NotFoundError < KeyError
+  # @param [String|Symbol|Object]
+  def initialize(key)
+    @key = key
+  end
+
+  # @return [String|Symbol|Object]
+  attr_reader :key
+
+  def message
+    "invalid sequence name: #{key.inspect}"
+  end
+end
