5467c191f0abeb789f1f8215589d5efc4da21742
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-12-26 18:59:26 +0100

    file (spec) add some instance methods examples

--- spec/alluvion/file_spec.rb
+++ spec/alluvion/file_spec.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# instance methdos --------------------------------------------------
+describe Alluvion::File, :'alluvion/file' do
+  let(:subject) { described_class.new(__FILE__) }
+
+  it { expect(subject).to respond_to(:ctime).with(0).arguments }
+  it { expect(subject).to respond_to(:mtime).with(0).arguments }
+
+  it { expect(subject).to respond_to(:mime).with(0).arguments }
+  it { expect(subject).to respond_to(:mime_type).with(0).arguments }
+end
+
 describe Alluvion::File, :'alluvion/file' do
   sham!(:samples).files.each do |fp, mime_type|
     context '.mime_type' do
