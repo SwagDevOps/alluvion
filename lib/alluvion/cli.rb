13e6be9bed0ac51f55f51cefb9a5a99aa8f7dce7
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-12-31 13:36:12 +0100

    cli (lib) use name class as namespace for commands + use command as base class

--- lib/alluvion/cli.rb
+++ lib/alluvion/cli.rb
@@ -22,7 +22,7 @@ class Alluvion::Cli
   # @formatter:off
   {
     Command: 'command',
-    UpCommand: 'up_command'
+    SynchroCommand: 'synchro_command'
   }.each do |s, fp|
     autoload(s, Pathname.new(__dir__).join("cli/#{fp}"))
 
@@ -48,20 +48,39 @@ class Alluvion::Cli
 
   protected
 
+  # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+
+  # Build callable commands.
+  #
   # @return [Class|Thor]
   #
   # @see Thor.register()
   # @see https://github.com/erikhuda/thor/blob/99330185faa6ca95e57b19a402dfe52b1eba8901/lib/thor.rb#L30
   def callable
-    self.class.__send__(:autoload, :Thor, 'thor')
+    require 'dry/inflector'
 
-    Class.new(Thor).tap do |klass|
+    Class.new(Command).tap do |klass|
       self.commands.each do |command|
-        command.commands.to_h.each do |name, c|
-          klass.__send__(:desc, c.usage, c.description, c.options)
-          klass.define_method(name) { |*args| invoke(c, args) }
+        Dry::Inflector.new.underscore(command.name.split('::').last).split('_')[0..-2].fetch(0).tap do |ns| # rubocop:disable Layout/LineLength
+          command.commands.to_h.each do |name, c|
+            klass.__send__(:desc, "#{ns}:#{name}", c.description)
+            # @formatter:off
+            c.options.each do |option_name, v|
+              {
+                aliases: v.aliases,
+                desc: v.description,
+                required: v.required,
+                type: v.type,
+                repeatable: v.repeatable,
+              }.tap { |option| klass.__send__(:option, option_name, option) }
+            end
+            # @formatter:on
+
+            klass.define_method("#{ns}:#{name}") { |*args| invoke(c, args) }
+          end
         end
       end
     end
   end
+  # rubocop:enable Metrics/AbcSize,Metrics/MethodLength
 end
