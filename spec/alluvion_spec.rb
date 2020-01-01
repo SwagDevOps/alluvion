5000cac7507755988ab11273bdf015dfc6f339bf
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-01-01 14:14:04 +0100

    alluvion (spec) add some constants

--- spec/alluvion_spec.rb
+++ spec/alluvion_spec.rb
@@ -3,8 +3,10 @@
 # constants ---------------------------------------------------------
 describe Alluvion, :alluvion do
   it { expect(described_class).to be_const_defined(:VERSION) }
+  it { expect(described_class).to be_const_defined(:Cli) }
   it { expect(described_class).to be_const_defined(:Config) }
   it { expect(described_class).to be_const_defined(:File) }
+  it { expect(described_class).to be_const_defined(:FileLock) }
   it { expect(described_class).to be_const_defined(:Host) }
   it { expect(described_class).to be_const_defined(:Shell) }
   it { expect(described_class).to be_const_defined(:Synchro) }
