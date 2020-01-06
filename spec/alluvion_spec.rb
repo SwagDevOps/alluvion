e9904923330eb66f6465c83c5ba6f718d08f8d4d
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-01-06 16:57:33 +0100

    alluvion (spec) update constants related examples

--- spec/alluvion_spec.rb
+++ spec/alluvion_spec.rb
@@ -5,6 +5,7 @@ describe Alluvion, :alluvion do
   it { expect(described_class).to be_const_defined(:VERSION) }
   it { expect(described_class).to be_const_defined(:Cli) }
   it { expect(described_class).to be_const_defined(:Config) }
+  it { expect(described_class).to be_const_defined(:ConfigFile) }
   it { expect(described_class).to be_const_defined(:File) }
   it { expect(described_class).to be_const_defined(:FileLock) }
   it { expect(described_class).to be_const_defined(:Host) }
