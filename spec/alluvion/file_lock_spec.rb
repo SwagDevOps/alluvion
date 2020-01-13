2b5f1b5d207beb4c2ba470bc7d91e6400ffac7b2
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-01-13 08:55:58 +0100

    file_lock (spec) update + improvements

--- spec/alluvion/file_lock_spec.rb
+++ spec/alluvion/file_lock_spec.rb
@@ -3,30 +3,40 @@
 autoload(:FileUtils, 'fileutils')
 autoload(:Pathname, 'pathname')
 
+# rubocop:disable Layout/LineLength
 describe Alluvion::FileLock, :'alluvion/file_lock' do
-  sham!(:configs).complete['locks']['up'].yield_self do |file|
-    subject { described_class.new(file, timeout: 0.15) }
-  end
+  after(:each) { subject.unlock }
 
-  before(:each) { sleep(0.5) }
-  after(:each) { sleep(0.5) }
+  let(:subject) do
+    sham!(:configs).complete['locks']['up'].yield_self do |file|
+      described_class.new(file)
+    end
+  end
 
   it { expect(subject).to be_a(Pathname) }
   it { expect(subject).to respond_to(:unlock).with(0).arguments }
   it { expect(subject.method(:call).original_name).to eq(:lock!) }
-  specify do
-    expect { |b| subject.public_send(:call, &b) }.to yield_with_no_args
+
+  context '.call' do
+    specify do
+      expect { |b| subject.public_send(:call, &b) }.to yield_with_no_args
+    end
+  end
+
+  context '.unlock' do
+    it { expect(subject.unlock).to be_a(described_class) }
   end
 end
 
 # Examples for locking -----------------------------------------------
-describe Alluvion::FileLock, :'alluvion/file_lock' do
-  sham!(:configs).complete['locks']['up'].tap do |file|
-    subject { described_class.new(file, timeout: 0.15) }
-  end
+describe Alluvion::FileLock, :'alluvion/file_lock', :'alluvion/file_lock#call' do
+  after(:each) { subject.unlock }
 
-  before(:each) { sleep(0.5) }
-  after(:each) { sleep(0.5) }
+  let(:subject) do
+    sham!(:configs).complete['locks']['up'].yield_self do |file|
+      described_class.new(file)
+    end
+  end
 
   [:lock!, :call].each do |method_name|
     context "##{method_name}" do
@@ -37,23 +47,31 @@ describe Alluvion::FileLock, :'alluvion/file_lock' do
   end
 end
 
-# rubocop:disable Layout/LineLength
 describe Alluvion::FileLock, :'alluvion/file_lock', :'alluvion/file_lock#parallel' do
-  sham!(:configs).complete['locks']['up'].tap do |file|
-    subject { described_class.new(file, timeout: 0.01) }
+  after(:each) { subject.unlock }
+
+  let(:subject) do
+    sham!(:configs).complete['locks']['up'].yield_self { |file| described_class.new(file) }
   end
 
-  after(:each) { sleep(0.5) }
+  let(:locker) do
+    lambda do |lock, &block|
+      SafeThread.new { described_class.new(lock).call }.tap do
+        sleep(0.01)
+        lock.call(&block)
+      end.join
+    end
+  end
 
   4.times do
-    { call: 0.01 }.each do |method_name, duration|
+    { call: 0.15 }.each do |method_name, duration|
       context "##{method_name}" do
         it 'in parallel run' do
-          lambda do
-            parallel(64) { subject.public_send(method_name, &-> { sleep(duration) }) }
-          end.tap do |callable|
-            expect { callable.call }.to raise_error(Alluvion::FileLock::Error).with_message('Can not acquire lock (up)')
-          end
+          expect do
+            locker.call(subject) do
+              subject.public_send(method_name, &-> { sleep(duration) })
+            end
+          end.to raise_error(Alluvion::FileLock::Error).with_message('Can not acquire lock (up)')
         end
       end
     end
