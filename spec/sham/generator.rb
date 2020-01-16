78d733c75921ad2b4f3b9c6b6ac74350111d45d5
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-01-16 08:37:46 +0100

    sham (spec) generator introduced

---
+++ spec/sham/generator.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+autoload(:SecureRandom, 'securerandom')
+
+Sham.config(FactoryStruct, :generator) do |c|
+  # rubocop:disable Layout/LineLength
+  c.attributes do
+    # @formatter:off
+    {
+      counter: ->(count) { (1..count).to_a },
+      randomizer: ->(count, size: 12) { (1..count).map { SecureRandom.urlsafe_base64(size) } }
+    }
+    # @formatter:on
+  end
+  # rubocop:enable Layout/LineLength
+end
