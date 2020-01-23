bbad4e90bc01fa60d118f2980e4c8f102f8ec9e2
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-01-23 18:26:19 +0100

    synchro (spec) update following previous changes #42f1db1

--- spec/alluvion/synchro_spec.rb
+++ spec/alluvion/synchro_spec.rb
@@ -5,17 +5,22 @@ describe Alluvion::Synchro, :'alluvion/synchro' do
   it { expect(described_class).to be_const_defined(:Sequence) }
 end
 
+# insrance methods --------------------------------------------------
 describe Alluvion::Synchro, :'alluvion/synchro' do
   let(:subject) { described_class.new(sham!(:configs).complete) }
 
-  [:up, :down].sort.each do |method_name|
-    it { expect(subject).to respond_to(method_name).with(0).arguments }
+  it { expect(subject).to respond_to(:call).with(1).arguments }
+end
 
-    context ".#{method_name}" do
+describe Alluvion::Synchro, :'alluvion/synchro' do
+  let(:subject) { described_class.new(sham!(:configs).complete) }
+
+  [:up, :down].sort.each do |direction|
+    context ".call(#{direction.inspect})" do
       it do
         # rubocop:disable Metrics/LineLength
         %r{^Can not connect to "[a-z]+://[a-z]+@[a-z]+.[a-z]+:[0-9]+"$}.tap do |msg|
-          expect { subject.public_send(method_name) }.to raise_error(RuntimeError).with_message(msg)
+          expect { subject.call(direction) }.to raise_error(RuntimeError).with_message(msg)
         end
         # rubocop:enable Metrics/LineLength
       end
