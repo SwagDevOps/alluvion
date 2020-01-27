daeb05aa74ef504d59e6b513632cc96721c9095e
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-01-27 10:11:02 +0100

    sham (spec) configs add `user` config

--- spec/sham/configs.rb
+++ spec/sham/configs.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 autoload(:SecureRandom, 'securerandom')
+autoload(:Etc, 'etc')
 
 # @formatter:off
 # noinspection RubyStringKeysInHashInspection
@@ -26,12 +27,21 @@ autoload(:SecureRandom, 'securerandom')
         'todo' => '/var/user/todo',
       }
     }
+  },
+  user: {
+    'user' => {
+      'HOME' => '<%= @HOME %>',
+      'UID' => '<%= @UID %>',
+      'USER' => '<%= @USER %>',
+      'SECRET' => '<%= @SECRET %>',
+    }
   }
 }.yield_self do |config|
   Sham.config(FactoryStruct, :configs) do |c|
     c.attributes do
       {
-        complete: config[:complete]
+        complete: config[:complete],
+        user: config[:user],
       }
     end
   end
